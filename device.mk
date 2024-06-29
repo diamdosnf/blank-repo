2a2db81eff8c5def8a1d7c456c728c72a0eddb50
NoNameBuilder <163744362+NoNameBuilder@users.noreply.github.com>
2024-06-29 18:24:06 +0300

    add files via upload

---
+++ device.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a24
+
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
